51ea6adf3f098739d55ff8f3049f5a395c12fc61
Fedjmike <fedjmike@gmail.com>
2013-06-30 20:42:52 +0100

    Fixed a vector bug and exanded vector interface

--- inc/vector.h
+++ inc/vector.h
@@ -7,10 +7,24 @@ typedef void (*dtorType)(void*); ///For use with vectorDestroyObjs
 typedef void* (*mapType)(void*); ///For use with vectorMap
 
 vector vectorCreate (int initialCapacity);
+
+/**
+ * Cleans up resources allocated by the vector but not the vector itself.
+ */
 void vectorDestroy (vector* v);
 void vectorDestroyObjs (vector* v, void (*dtor)(void*));
 
 void vectorAdd (vector* v, void* item);
+
+/**
+ * Returns the value, or null if out of bounds.
+ */
+void* vectorGet (const vector* v, int n);
+
+/**
+ * Attempts to set an index to a value. Returns non-zero on failure.
+ */
+int vectorSet (vector* v, int n, void* value);
 
 /**
  * Maps dest[n] to f(src[n]) for n in src->length.
@@ -18,4 +32,4 @@ void vectorAdd (vector* v, void* item);
  * src and dest can match. Lengths not checked.
  * @see mapType
  */
-void vectorMap (vector* dest, vector* src, void* (*f)(void*));
+void vectorMap (vector* dest, void* (*f)(void*), vector* src);
--- src/vector.c
+++ src/vector.c
@@ -1,6 +1,9 @@
 #include "../inc/vector.h"
 
+#include "../std/std.h"
+
 #include "stdlib.h"
+#include "stdio.h"
 
 vector vectorCreate (int initialCapacity) {
     vector v;
@@ -12,23 +15,40 @@ vector vectorCreate (int initialCapacity) {
 
 void vectorDestroy (vector* v) {
     free(v->buffer);
-    free(v);
+    v->buffer = 0;
 }
 
 void vectorDestroyObjs (vector* v, dtorType dtor) {
     /*This will mess up the vector, watevs*/
-    vectorMap(v, v, (mapType) dtor);
+    vectorMap(v, (mapType) dtor, v);
     vectorDestroy(v);
 }
 
 void vectorAdd (vector* v, void* item) {
     if (v->length == v->capacity)
-        v->buffer = realloc(v->buffer, v->capacity *= 2);
+        v->buffer = realloc(v->buffer, (v->capacity *= 2)*sizeof(void*));
 
     v->buffer[v->length++] = item;
 }
 
-void vectorMap (vector* dest, vector* src, mapType f) {
+void* vectorGet (const vector* v, int n) {
+    if (n < v->length)
+        return v->buffer[n];
+
+    else
+        return 0;
+}
+
+int vectorSet (vector* v, int n, void* value) {
+    if (n < v->length) {
+        v->buffer[n] = value;
+        return false;
+
+    } else
+        return true;
+}
+
+void vectorMap (vector* dest, mapType f, vector* src) {
     for (int n = 0; n < src->length; n++)
         dest->buffer[n] = f(src->buffer[n]);
 }
