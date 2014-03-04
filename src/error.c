a63a77b5e87bba9c68a85089cec2f4e94a5bd889
Fedjmike <sam.nipps@gmail.com>
2014-03-04 23:01:17 +0000

    Colourful error messages

--- src/error.c
+++ src/error.c
@@ -12,18 +12,86 @@
 #include "stdarg.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
+
+const char* consoleNormal  = "\033[1;0m";
+const char* consoleRed     = "\033[1;31m";
+const char* consoleGreen   = "\033[1;32m";
+const char* consoleYellow  = "\033[1;33m";
+const char* consoleBlue    = "\033[1;34m";
+const char* consoleMagenta = "\033[1;35m";
+const char* consoleCyan    = "\033[1;36m";
+const char* consoleWhite   = "\033[1;37m";
 
 static void tokenLocationMsg (tokenLocation loc) {
     printf("%s:%d:%d: ", loc.filename, loc.line, loc.lineChar);
 }
 
-static void parserError (parserCtx* ctx, const char* format, ...) {
+static void errorHighlighted (const char* format, va_list args) {
+    const char* colourString = consoleWhite;
+    const char* colourNumber = consoleMagenta;
+    const char* colourOp = consoleCyan;
+    const char* colourType = consoleGreen;
+    const char* colourIdent = consoleWhite;
+
+    int flen = strlen(format);
+
+    for (int i = 0; i < flen; i++) {
+        if (format[i] == '$') {
+            /*Regular string*/
+            if (format[i+1] == 's')
+                printf("%s", va_arg(args, char*));
+
+            /*Highlighted string*/
+            else if (format[i+1] == 'h')
+                printf("%s%s%s", colourString, va_arg(args, char*), consoleNormal);
+
+            /*Operator*/
+            else if (format[i+1] == 'o')
+                printf("%s%s%s", colourOp, va_arg(args, char*), consoleNormal);
+
+            /*Integer*/
+            else if (format[i+1] == 'd')
+                printf("%s%d%s", colourNumber, va_arg(args, int), consoleNormal);
+
+            /*Raw type*/
+            else if (format[i+1] == 't') {
+                char* typeStr = typeToStr(va_arg(args, type*), "");
+                printf("%s%s%s", colourType, typeStr, consoleNormal);
+                free(typeStr);
+
+            /*Type with symbol name*/
+            } else if (format[i+1] == 'n') {
+                type* DT = va_arg(args, type*);
+                char* ident = va_arg(args, char*);
+                char* identStr = malloc(strlen(colourIdent)+strlen(ident)+strlen(colourType)+1);
+                sprintf(identStr, "%s%s%s", colourIdent, ident, colourType);
+                char* typeStr = typeToStr(DT, identStr);
+                printf("%s%s%s", colourType, typeStr, consoleNormal);
+                free(identStr);
+                free(typeStr);
+
+            } else if (format[i+1] == '\0')
+                break;
+
+            /*Unknown, ignore*/
+            else
+                {}
+
+            i++;
+
+        } else
+            putchar(format[i]);
+    }
+}
+
+static void errorParser (parserCtx* ctx, const char* format, ...) {
     tokenLocationMsg(ctx->location);
-    printf("error: ");
+    printf("%serror%s: ", consoleRed, consoleNormal);
 
     va_list args;
     va_start(args, format);
-    vprintf(format, args);
+    errorHighlighted(format, args);
     va_end(args);
 
     putchar('\n');
@@ -32,13 +100,13 @@ static void parserError (parserCtx* ctx, const char* format, ...) {
     debugWait();
 }
 
-static void analyzerError (analyzerCtx* ctx, const ast* Node, const char* format, ...) {
+static void errorAnalyzer (analyzerCtx* ctx, const ast* Node, const char* format, ...) {
     tokenLocationMsg(Node->location);
-    printf("error: ");
+    printf("%serror%s: ", consoleRed, consoleNormal);
 
     va_list args;
     va_start(args, format);
-    vprintf(format, args);
+    errorHighlighted(format, args);
     va_end(args);
 
     putchar('\n');
@@ -50,32 +118,32 @@ static void analyzerError (analyzerCtx* ctx, const ast* Node, const char* format
 /*:::: PARSER ERRORS ::::*/
 
 void errorExpected (parserCtx* ctx, const char* expected) {
-    parserError(ctx, "expected %s, found '%s'", expected, ctx->lexer->buffer);
+    errorParser(ctx, "expected $h, found '$h'", expected, ctx->lexer->buffer);
 }
 
 void errorUndefSym (parserCtx* ctx) {
-    parserError(ctx, "'%s' undefined", ctx->lexer->buffer);
+    errorParser(ctx, "'$h' undefined", ctx->lexer->buffer);
 }
 
 void errorUndefType (parserCtx* ctx) {
-    parserError(ctx, "'%s' undefined, expected type", ctx->lexer->buffer);
+    errorParser(ctx, "'$h' undefined, expected type", ctx->lexer->buffer);
 }
 
 void errorIllegalOutside (parserCtx* ctx, const char* what, const char* where) {
-    parserError(ctx, "illegal %s outside of %s", what, where);
+    errorParser(ctx, "illegal $s outside of $s", what, where);
 }
 
 void errorRedeclaredSymAs (parserCtx* ctx, const sym* Symbol, symTag tag) {
     const ast* first = (const ast*) vectorGet(&Symbol->decls, 0);
 
-    parserError(ctx, "'%s' redeclared as %s", Symbol->ident, symTagGetStr(tag));
+    errorParser(ctx, "'$h' redeclared as $h", Symbol->ident, symTagGetStr(tag));
 
     tokenLocationMsg(first->location);
-    printf("first declaration here as %s\n", symTagGetStr(Symbol->tag));
+    printf("first declaration here as $h\n", symTagGetStr(Symbol->tag));
 }
 
 void errorReimplementedSym (parserCtx* ctx, const sym* Symbol) {
-    parserError(ctx, "%s '%s' reimplemented",
+    errorParser(ctx, "$h '$h' reimplemented",
           Symbol->tag == symId ? "function" : symTagGetStr(Symbol->tag), Symbol->ident);
 
     tokenLocationMsg(Symbol->impl->location);
@@ -83,85 +151,61 @@ void errorReimplementedSym (parserCtx* ctx, const sym* Symbol) {
 }
 
 void errorFileNotFound (parserCtx* ctx, const char* name) {
-    parserError(ctx, "file not found, '%s'", name);
+    errorParser(ctx, "file not found, '$h'", name);
 }
 
 /*:::: ANALYZER ERRORS ::::*/
 
 void errorTypeExpected (analyzerCtx* ctx, const ast* Node, const char* where, const char* expected) {
-    char* found = typeToStr(Node->dt, "");
-    analyzerError(ctx, Node, "%s requires %s, found '%s'", where, expected, found);
-    free(found);
+    errorAnalyzer(ctx, Node, "$s requires $s, found $t", where, expected, Node->dt);
 }
 
 void errorTypeExpectedType (analyzerCtx* ctx, const ast* Node, const char* where, const type* expected) {
-    char* expectedStr = typeToStr(expected, "");
-    errorTypeExpected(ctx, Node, where, expectedStr);
-    free(expectedStr);
+    errorAnalyzer(ctx, Node, "$s requires $t, found $t", where, expected, Node->dt);
 }
 
 void errorLvalue (analyzerCtx* ctx, const ast* Node, const char* o) {
-    analyzerError(ctx, Node, "%s requires lvalue", o);
+    errorAnalyzer(ctx, Node, "$o requires lvalue", o);
 }
 
 void errorMismatch (analyzerCtx* ctx, const ast* Node, const char* o) {
-    char* L = typeToStr(Node->l->dt, "");
-    char* R = typeToStr(Node->r->dt, "");
-    analyzerError(ctx, Node, "type mismatch between %s and %s for %s", L, R, o);
-    free(L);
-    free(R);
+    errorAnalyzer(ctx, Node, "type mismatch between $t and $t for $o",
+                  Node->l->dt, Node->r->dt, o);
 }
 
 void errorDegree (analyzerCtx* ctx, const ast* Node,
                   const char* thing, int expected, int found, const char* where) {
-    analyzerError(ctx, Node, "%s expected %d %s, %d given", where, expected, thing, found);
+    errorAnalyzer(ctx, Node, "$h expected $d $s, $d given", where, expected, thing, found);
 }
 
 void errorParamMismatch (analyzerCtx* ctx, const ast* Node,
                          int n, const type* expected, const type* found) {
-    char* expectedStr = typeToStr(expected, "");
-    char* foundStr = typeToStr(found, "");
-    analyzerError(ctx, Node, "type mismatch at parameter %d, %s: found %s",
-                  n+1, expectedStr, foundStr);
-    free(expectedStr);
-    free(foundStr);
+    errorAnalyzer(ctx, Node, "type mismatch at parameter $d, $t: found $t",
+                  n+1, expected, found);
 }
 
 void errorNamedParamMismatch (analyzerCtx* ctx, const ast* Node,
                               int n, const sym* fn, const type* found) {
-    char* foundStr = typeToStr(found, "");
     const sym* param = symGetChild(fn, n);
-    char* paramStr = typeToStr(param->dt, param->ident ? param->ident : "");
-    analyzerError(ctx, Node, "type mismatch at parameter %d of %s, %s: found %s",
-                  n+1, fn->ident, paramStr, foundStr);
-    free(foundStr);
-    free(paramStr);
+    errorAnalyzer(ctx, Node, "type mismatch at parameter $d of $h, $n: found $t",
+                  n+1, fn->ident, param->dt, param->ident ? param->ident : "", found);
 }
 
 void errorMember (analyzerCtx* ctx, const char* o, const ast* Node, const type* record) {
-    char* recordStr = typeToStr(record, "");
-    analyzerError(ctx, Node, "%s expected field of %s, found %s", o, recordStr, Node->literal);
-    free(recordStr);
+    errorAnalyzer(ctx, Node, "$o expected field of $t, found $h", o, record, Node->literal);
 }
 
 void errorInitFieldMismatch (analyzerCtx* ctx, const ast* Node,
                              const sym* structSym, const sym* fieldSym) {
-    char* fieldStr = typeToStr(fieldSym->dt, fieldSym->ident);
-    char* foundStr = typeToStr(Node->dt,   (Node->symbol && Node->symbol->ident)
-                                         ? Node->symbol->ident : "");
-    analyzerError(ctx, Node, "type mismatch: %s given for initialization of field %s in %s %s",
-                  foundStr, fieldStr, symTagGetStr(structSym->tag), structSym->ident);
-    free(fieldStr);
-    free(foundStr);
+    errorAnalyzer(ctx, Node, "type mismatch: $n given for initialization of field $n in $h $h",
+                  Node->dt, (Node->symbol && Node->symbol->ident) ? Node->symbol->ident : "",
+                  fieldSym->dt, fieldSym->ident,
+                  symTagGetStr(structSym->tag), structSym->ident);
 }
 
 void errorConflictingDeclarations (analyzerCtx* ctx, const ast* Node,
                                    const sym* Symbol, const type* found) {
-    char* expectedStr = typeToStr(Symbol->dt, Symbol->ident);
-    char* foundStr = typeToStr(found, "");
-    analyzerError(ctx, Node, "%s redeclared as conflicting type %s", expectedStr, foundStr);
-    free(expectedStr);
-    free(foundStr);
+    errorAnalyzer(ctx, Node, "$n redeclared as conflicting type $t", Symbol->dt, Symbol->ident, found);
 
     for (int n = 0; n < Symbol->decls.length; n++) {
         const ast* Current = (const ast*) vectorGet(&Symbol->decls, n);
@@ -174,13 +218,11 @@ void errorConflictingDeclarations (analyzerCtx* ctx, const ast* Node,
 }
 
 void errorRedeclared (analyzerCtx* ctx, const ast* Node, const sym* Symbol) {
-    if (Symbol->tag == symId || Symbol->tag == symParam) {
-        char* symStr = typeToStr(Symbol->dt, Symbol->ident);
-        analyzerError(ctx, Node, "%s redeclared", symStr);
-        free(symStr);
+    if (Symbol->tag == symId || Symbol->tag == symParam)
+        errorAnalyzer(ctx, Node, "$n redeclared", Symbol->dt, Symbol->ident);
 
-    } else
-        analyzerError(ctx, Node, "%s %s redeclared", symTagGetStr(Symbol->tag), Symbol->ident);
+    else
+        errorAnalyzer(ctx, Node, "$h $h redeclared", symTagGetStr(Symbol->tag), Symbol->ident);
 
     for (int n = 0; n < Symbol->decls.length; n++) {
         const ast* Current = (const ast*) vectorGet(&Symbol->decls, n);
@@ -194,17 +236,13 @@ void errorRedeclared (analyzerCtx* ctx, const ast* Node, const sym* Symbol) {
 }
 
 void errorIllegalSymAsValue (analyzerCtx* ctx, const ast* Node, const sym* Symbol) {
-    char* SymStr = typeToStr(Symbol->dt, Symbol->ident);
-    analyzerError(ctx, Node, "cannot use %s %s as a value", symTagGetStr(Symbol->tag), SymStr);
-    free(SymStr);
+    errorAnalyzer(ctx, Node, "cannot use $h $n as a value", symTagGetStr(Symbol->tag), Symbol->dt, Symbol->ident);
 }
 
 void errorCompileTimeKnown (analyzerCtx* ctx, const ast* Node, const sym* Symbol, const char* what) {
-    char* SymStr = typeToStr(Symbol->dt, Symbol->ident);
-    analyzerError(ctx, Node, "declaration of %s needed a compile-time known %s", SymStr, what);
-    free(SymStr);
+    errorAnalyzer(ctx, Node, "declaration of $h needed a compile-time known $s", Symbol->ident, what);
 }
 
 void errorCompoundLiteralWithoutType (struct analyzerCtx* ctx, const struct ast* Node) {
-    analyzerError(ctx, Node, "compound literal without explicit type");
+    errorAnalyzer(ctx, Node, "compound literal without explicit type");
 }
