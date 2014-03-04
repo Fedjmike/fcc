3f023d854d9e1819a872a88d66ffa4cd126e5598
Fedjmike <sam.nipps@gmail.com>
2014-03-04 23:03:00 +0000

    eval.c: member ops

--- src/eval.c
+++ src/eval.c
@@ -51,7 +51,8 @@ static evalResult evalBOP (const architecture* arch, ast* Node) {
     evalResult L = eval(arch, Node->l),
                R = eval(arch, Node->r);
 
-    if (!strcmp(Node->o, ",") || !strcmp(Node->o, "="))
+    if (   !strcmp(Node->o, ",") || !strcmp(Node->o, "=")
+        || !strcmp(Node->o, "->") || !strcmp(Node->o, "."))
         return R;
 
     else if (!strcmp(Node->o, "&&")) {
@@ -167,8 +168,8 @@ static evalResult evalLiteral (const architecture* arch, ast* Node) {
 
     /*Only enum constants are known at compile time*/
     else if (Node->litTag == literalIdent)
-        return (evalResult) {Node->symbol->tag == symEnumConstant,
-                             Node->symbol->constValue};
+        return (evalResult) {Node->symbol && Node->symbol->tag == symEnumConstant,
+                             (int)(Node->symbol && Node->symbol->constValue)};
 
     else if (   Node->litTag == literalStr
              || Node->litTag == literalCompound
