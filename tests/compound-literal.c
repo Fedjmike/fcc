27c9ba6efd730becb8dd32fd58c81fb409c3c9f3
Fedjmike <sam.nipps@gmail.com>
2014-03-31 20:13:20 +0100

    More complicated compound literal test

--- tests/compound-literal.c
+++ tests/compound-literal.c
@@ -1,8 +1,20 @@
+using "stdio.h";
+
 typedef struct A {
     int x, y;
 } A;
 
+int f (int *p, int n) {
+	for (int i = 0; i < n; i++)
+		printf("%d  ", p[i]);
+		
+	puts("");
+}
+
 int main () {
     A a = (A) {0, 0};
+	
+	f((int[3]) {0, 1, 2}, 3);
+		
     return 0;
 }
