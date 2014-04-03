e755e08fe587259e78e41952a5a43fe22b0a8a1d
Fedjmike <sam.nipps@gmail.com>
2014-04-03 18:25:32 +0100

    Quieten selfbuild.sh

--- selfbuild.sh
+++ selfbuild.sh
@@ -4,10 +4,10 @@ PATTERN="vector|architecture|ast|sym|type|parser.*|analyzer.*|emittEer.*|eval|co
 
 set -e
 
-pushd obj/self
+pushd obj/self >/dev/null
 HOSTING=`find ../../src/*.c | egrep "$PATTERN"`
 ../../bin/$CONFIG/fcc -I ../../tests/include -c $HOSTING
-popd
+popd >/dev/null
 
 EXT=`find src/*.c | egrep -v "$PATTERN"`
 gcc -m32 $CFLAGS $EXT obj/self/*.o -o bin/self/fcc
