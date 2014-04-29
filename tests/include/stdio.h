54affe0528e4eacc1ad78b82788405e7b8952078
Fedjmike <sam.nipps@gmail.com>
2014-04-29 23:04:17 +0100

    stdio update

--- tests/include/stdio.h
+++ tests/include/stdio.h
@@ -3,10 +3,15 @@ using "_stdarg.h";
 struct FILE;
 
 FILE* fopen (const char*, const char*);
-int fclose (FILE*); 
+int fclose (FILE*);
+
 int fprintf (FILE*, const char*, ...);
 int vfprintf (FILE*, const char*, _va_list);
 int fputc (int, FILE*);
+
+int fseek (FILE*, int, int);
+int feof (FILE*);
+int fgetc (FILE*);
 
 int printf (const char*, ...);
 int puts (const char*);
@@ -15,3 +20,7 @@ int putchar (int);
 int sprintf (char*, const char*, ...);
 
 extern FILE *stdout, *stdin, *stderr;
+
+enum {
+	SEEK_SET = 0, SEEK_CUR = 1, SEEK_END = 2
+};
